ceaee3f37810d8144868bb128d8ac0a528be537c
Pierre Muller <pierremanu.muller@gmail.com>
2021-07-01 17:35:31 +0200

    change Contact to smart comonent and Online state modifier

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Contact.css';
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img src={props.avatar} className="avatar" alt={props.name} />
-            <div>
-                <h4 className="name">{props.name}</h4>
-                <div className="status">
-                    <span className={props.online ? 'status-online' : 'status-offline'} ></span>
-                    <p className="status-text">{props.online ? 'Online' : 'Offline'}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.online,
+        };
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img src={this.props.avatar} className="avatar" alt={this.props.name} />
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div 
+                        className="status"
+                        onClick={event => {
+                            const newOnline = !this.state.online;
+                            this.setState({online: newOnline});
+                        }}
+                    >
+                        <span className={this.state.online ? 'status-online' : 'status-offline'} ></span>
+                        <p className="status-text">{this.state.online ? 'Online' : 'Offline'}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 Contact.propTypes = {
